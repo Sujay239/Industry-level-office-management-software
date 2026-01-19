6b3d4fcef689e15566d90bdfd44ccd4b2856d360
Sujay239 <sujaykumarkotal8520@gmail.com>
2026-01-19 14:00:08 +0530

    Whole fixed code for depoyment

---
+++ Office-management-railway-deployment/routes/chat.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { getChats, getMessages, getOrCreateDirectChat, getUsers, createChat, markMessagesRead as markMessagesAsReadController, makeAdmin, removeMember, addMembers, leaveChat } from '../controllers/chatController.js';
+import { authenticateToken } from '../middlewares/authenticateToken.js';
+
+const router = express.Router();
+
+router.get('/', getChats);
+
+router.get('/:chatId/messages', getMessages);
+
+router.post('/dm', getOrCreateDirectChat);
+
+router.get('/users', getUsers);
+
+router.post('/create', createChat);
+
+router.post('/mark-read', markMessagesAsReadController);
+
+router.post('/:chatId/add-members', addMembers);
+
+router.post('/:chatId/make-admin', makeAdmin);
+
+router.post('/:chatId/remove-member', removeMember);
+
+router.post('/:chatId/leave', leaveChat);
+
+export default router;
